2008afa9c0c8bd04f3afc32fcee4be2084a0d411
류형욱 <mintrupt@mintrupt.com>
2014-09-02 12:54:01 +0900

    led-test/test2.js에 주석을 추가합니다.

--- led-test/test2.js
+++ led-test/test2.js
@@ -1,3 +1,4 @@
+// Arduino 라이브러리 duino를 이용하여 온 보드 LED를 제어합니다.
 var arduino = require('duino');
 
 var board = new arduino.Board({
@@ -8,6 +9,5 @@ var led = new arduino.Led({
   board: board
 });
 
+// 500ms로 LED를 점멸하도록 합니다.
 led.blink(500);
-
-
