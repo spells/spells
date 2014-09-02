b6acfea7f2b950265d6d4d61d206543430bd2b73
류형욱 <mintrupt@mintrupt.com>
2014-09-02 12:54:37 +0900

    led-test/testApp/routes/index.js에 주석을 추가합니다.

--- led-test/testApp/routes/index.js
+++ led-test/testApp/routes/index.js
@@ -8,22 +8,26 @@ var led = new arduino.Led({
   board: board
 });
 
+// 처음 LED 상태를 꺼짐 상태로 설정합니다.
 led.off();
 
 var ledStatus = false;
 
-/* GET home page. */
+// 현재 LED의 꺼짐 또는 켜짐 상태를 웹 페이지에 표시합니다.
 router.get('/', function(req, res) {
   res.render('index', { ledStatus: ledStatus ? 'ON' : 'OFF' });
 });
 
 router.get('/toggle', function (req, res) {
+  // LED 켜짐 상태에 따라 적절한 동작을 수행하고
   if (ledStatus) {
     led.off();
   } else {
     led.on();
   }
+  // LED 켜짐 상태를 반전시킵니다.
   ledStatus = !ledStatus;
+  // 첫 페이지로 리디렉트합니다.
   res.redirect('/');
 });
 
