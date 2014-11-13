39d882ab4707258d75c1d65ffaf08af91d4a8078
류형욱 <mintrupt@mintrupt.com>
2014-11-14 06:06:58 +0900

    capitalizer

---
+++ spells-messaging/spells-messaging-edge/lib/capitalizer.js
@@ -0,0 +1,19 @@
+module.exports = function () {
+  const pattern = new RegExp(/^[A-Za-z0-9]+$/);
+  return {
+    toCamelCase: function (name) {
+      var match = name.match(pattern);
+      if (!match || match[0] !== name) {
+        throw new Error();
+      }
+      return name.charAt(0).toLowerCase() + name.slice(1);
+    },
+    toPascalCase: function (name) {
+      var match = name.match(pattern);
+      if (!match || match[0] !== name) {
+        throw new Error();
+      }
+      return name.charAt(0).toUpperCase() + name.slice(1);
+    }
+  };
+};
