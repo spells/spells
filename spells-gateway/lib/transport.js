a3d039ef5dd0f591ba59a93fece5279538b2052a
류형욱 <mintrupt@mintrupt.com>
2014-11-17 01:16:30 +0900

    base64, armor

--- spells-gateway/lib/transport.js
+++ spells-gateway/lib/transport.js
@@ -7,12 +7,49 @@ module.exports = function () {
     return crc.crc24(value);
   };
 
-  var base64 = {
+  var base64 = {};
+  base64.encode = function (buffer) {
+    return buffer.toString('base64');
+  };
+  base64.decode = function (text) {
+    var buffer = new Buffer(text, 'base64');
+    if (base64.encode(buffer) !== text) {
+      throw new Error();
+    }
+    return buffer;
+  };
+
+  var crc24Buffer = function (value) {
+    var checksum = crc24(value);
+    var buffer = new Buffer(3);
+    buffer[0] = (checksum & 0x00FF0000) >> 16;
+    buffer[1] = (checksum & 0x0000FF00) >> 8;
+    buffer[2] = (checksum & 0x000000FF) >> 0;
+    return buffer;
+  };
+
+  var armor = {
     encode: function (buffer) {
-      return buffer;
+      var bodyBase64 = base64.encode(buffer);
+      var checksumBuffer = crc24Buffer(buffer);
+      var checksumBase64 = base64.encode(checksumBuffer);
+      return bodyBase64 + '?' + checksumBase64;
     },
     decode: function (text) {
-      return text;
+      text = text;
+      var q = text.indexOf('?');
+      var before = text.substr(0, q);
+      var after = text.substr(q + 1);
+      var beforeBuffer = base64.decode(before);
+      
+      var afterBuffer = base64.decode(after);
+      var checksumBuffer = crc24Buffer(beforeBuffer);
+      for (var i = 0; i < 3; i++) {
+        if (afterBuffer[i] !== checksumBuffer[i]) {
+          throw new Error();
+        }
+      }
+      return beforeBuffer;
     }
   };
 
@@ -20,6 +57,7 @@ module.exports = function () {
     checksum: {
       crc24: crc24
     },
-    base64: base64
+    base64: base64,
+    armor: armor
   };
 };
--- spells-gateway/test/transport.js
+++ spells-gateway/test/transport.js
@@ -12,14 +12,16 @@ describe('transport', function () {
   helper.checkFunctionExists(transport.checksum, 'crc24', 'checksum.crc24');
   helper.checkFunctionExists(transport.base64, 'encode', 'base64.encode');
   helper.checkFunctionExists(transport.base64, 'decode', 'base64.decode');
+  helper.checkFunctionExists(transport.armor, 'encode', 'armor.encode');
+  helper.checkFunctionExists(transport.armor, 'decode', 'armor.decode');
 
   describe('checksum', function () {
     describe('crc24', function () {
-      it('빈 버퍼에 대한 CRC24 값은 0xb704ce', function () {
+      it('빈 버퍼에 대한 결과는 0xb704ce', function () {
         var buffer = new Buffer([]);
         assert.strictEqual(transport.checksum.crc24(buffer), 0xb704ce);
       });
-      it('문자열 \"one\\0\"를 나타내는 버퍼 0x6F, 0x6E, 0x65, 0x00의 CRC24 값은 0x93f334', function () {
+      it('문자열 \"one\\0\"를 나타내는 버퍼 0x6F, 0x6E, 0x65, 0x00에 대한 결과는 0x93f334', function () {
         var data = [0x6F, 0x6E, 0x65, 0x00];
         var buffer = new Buffer(data);
         assert.strictEqual(transport.checksum.crc24(buffer), 0x93f334);
@@ -29,8 +31,96 @@ describe('transport', function () {
 
   describe('base64', function () {
     describe('encode', function () {
+      it('빈 버퍼에 대한 결과는 \"\"', function () {
+        var buffer = new Buffer([]);
+        assert.strictEqual(transport.base64.encode(buffer), '');
+      });
+      it('문자열 \"one\\0\"를 나타내는 버퍼 0x6F, 0x6E, 0x65, 0x00에 대한 결과는 b25lAA==', function () {
+        var data = [0x6F, 0x6E, 0x65, 0x00];
+        var buffer = new Buffer(data);
+        assert.strictEqual(transport.base64.encode(buffer), 'b25lAA==');
+      });
+      it('버퍼 0xB7, 0x04, 0xCE에 대한 결과는 twTO', function () {
+        var data = [0xB7, 0x04, 0xCE];
+        var buffer = new Buffer(data);
+        assert.strictEqual(transport.base64.encode(buffer), 'twTO');
+      });
+      it('버퍼 0x93, 0xF3, 0x34에 대한 결과는 k/M0', function () {
+        var data = [0x93, 0xF3, 0x34];
+        var buffer = new Buffer(data);
+        assert.strictEqual(transport.base64.encode(buffer), 'k/M0');
+      });
     });
     describe('decode', function () {
+      it('\"\"에 대한 결과는 빈 버퍼', function () {
+        var buffer = new Buffer([]);
+        assert.deepEqual(transport.base64.decode(''), buffer);
+      });
+      it('b25lAA==에 대한 결과는 버퍼 0x6F, 0x6E, 0x65, 0x00', function () {
+        var data = [0x6F, 0x6E, 0x65, 0x00];
+        var buffer = new Buffer(data);
+        assert.deepEqual(transport.base64.decode('b25lAA=='), buffer);
+      });
+      it('twTO에 대한 결과는 버퍼 0xB7, 0x04, 0xCE', function () {
+        var data = [0xB7, 0x04, 0xCE];
+        var buffer = new Buffer(data);
+        assert.deepEqual(transport.base64.decode('twTO'), buffer);
+      });
+      it('잘못된 입력이 주어지면 예외를 던져야 합니다.', function () {
+        assert.throws(function () {
+          transport.base64.decode('===');
+        });
+        assert.throws(function () {
+          transport.base64.decode('안녕하세요');
+        });
+        assert.throws(function () {
+          transport.base64.decode('-');
+        });
+      });
+    });
+  });
+
+  describe('armor', function () {
+    describe('encode', function () {
+      it('빈 버퍼에 대한 결과는 ?twTO', function () {
+        var buffer = new Buffer([]);
+        assert.strictEqual(transport.armor.encode(buffer), '?twTO');
+      });
+      it('버퍼 0x6F, 0x6E, 0x65, 0x00에 대한 결과는 b25lAA==?k/M0', function () {
+        var buffer = new Buffer([0x6F, 0x6E, 0x65, 0x00]);
+        assert.strictEqual(transport.armor.encode(buffer), 'b25lAA==?k/M0');
+      });
+    });
+    describe('decode', function () {
+      it('?twTO에 대한 결과는 빈 버퍼', function () {
+        var buffer = new Buffer([]);
+        assert.deepEqual(transport.armor.decode('?twTO'), buffer);
+      });
+      it('b25lAA==?k/M0에 대한 결과는 버퍼 0x6F, 0x6E, 0x65, 0x00', function () {
+        var buffer = new Buffer([0x6F, 0x6E, 0x65, 0x00]);
+        assert.deepEqual(transport.armor.decode('b25lAA==?k/M0'), buffer);
+      });
+      describe('잘못된 입력 테스트', function () {
+        var test = function (text) {
+          it('\"' + text + '\" 예외를 던져야 합니다.', function () {
+            assert.throws(function () {
+              transport.armor.decode(text);
+            });
+          });
+        };
+        test('');
+        test('?');
+        test('??');
+        test('b25lAA==?k/M0?');
+        test('b25lAA==??k/M0');
+        test('?b25lAA==?k/M0');
+        test('b25lAA==k/M0');
+        test('b25lAA==?k/M1');
+        test('b25lAB==?k/M0');
+        test('예외');
+        test('오류');
+        test('안녕하세요');
+      });
     });
   });
 });
